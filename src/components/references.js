830552724dd4782558e4ab2f466b610100547cf4
Kamil <kmirosz6@gmail.com>
2019-06-29 09:46:27 +0200

    mct some changes

---
+++ src/components/references.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import Img from 'gatsby-image'
+
+import { ContainerStyled } from "../components/Container.styled"
+import Header from "./sectionHeader"
+
+import fbLogo from '../images/fb-logo.png'
+import fbFind from '../images/find-us-fb.jpeg'
+
+export default class References extends Component {
+  static propTypes = {
+    prop: PropTypes
+  }
+
+  render() {
+    return (
+      <section className="references">
+        <ContainerStyled>
+          <Header title="DOŁĄCZ " subtitle="DO NAS " />
+        </ContainerStyled>
+        <ContainerStyled small>
+          <div className="references__wrrapper ">
+            <div className="references-block">
+              <img src={ fbLogo } alt="Logo z facebooka"/>
+              <p>
+                <span className="title">
+                  Mrągowskie centrum <br />Treningowe <span className="label text">
+                    + 500
+                  </span>
+                </span>
+              </p>
+              <div className="references__footer">
+                <a href="https://www.facebook.com/MCTreningowe/">
+                  <img src={fbFind} alt="Znajdź i polub nas na facebooku"/>
+                </a>
+              </div>
+            </div>
+            <div className="references-block references-block--big">
+              <Img className="references-block__bg" fixed={this.props.background} />
+            </div>
+          </div>
+        </ContainerStyled>
+      </section>
+    )
+  }
+}
